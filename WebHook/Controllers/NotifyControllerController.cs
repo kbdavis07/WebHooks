f68f8cc95c801a01f762dc03abd4e6ec256d3c95
kbdavis07 <kbdavis07@yahoo.com>
2017-03-09 00:16:38 -0500

    Saving Progress made so far.

--- WebHook/Controllers/NotifyControllerController.cs
+++ WebHook/Controllers/NotifyControllerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using WebHook.Models;
 
 namespace WebHook.Controllers
 {
@@ -13,8 +14,19 @@ namespace WebHook.Controllers
         [HttpPost]
         public async Task<ActionResult> Submit()
         {
+            Transaction NewTransaction = new Transaction
+            {
+                Id = 1,
+                OrderID = 1,
+                OrderDate = DateTime.Now,
+                OrderTotal = 20.00m,
+                Status = "Created"
+            
+            };
+
+          
             // Create an event with action 'event1' and additional data
-            await this.NotifyAsync("event1", new { P1 = "p1" });
+            await this.NotifyAsync("event1", new { P1 = NewTransaction });
 
             return new EmptyResult();
         }
---
+++ WebHook/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHook.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// PayPal invoice_number
+        /// </summary>
+        public int OrderID { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        
+        public decimal OrderTotal { get; set; }
+
+        public string Status { get; set; }
+        
+    }
+}
